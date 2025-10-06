6e04e64e2244357e7e4a04643f1cef08fe004062
Anh Duc Vu <AnhDuc.Vu@paluno.uni-due.de>
2025-10-06 16:42:37 +0000

    pinning dockerfile test uses fixed date now

--- tests/integration/dockerfiles/test_pin_dockerfile.py
+++ tests/integration/dockerfiles/test_pin_dockerfile.py
@@ -10,7 +10,10 @@ def test_pin_dockerfile(snapshot, resources_dir, tmp_path):
 
     dockerfile_path = resources_dir / "Dockerfile"
     dockerfile_contents = dockerfile_path.read_text()
-    pinned_dockerfile_contents = pin_dockerfile_base_image_and_packages(dockerfile_contents, datetime.now())
+
+    cutoff_date = datetime(2025, 1, 1)
+
+    pinned_dockerfile_contents = pin_dockerfile_base_image_and_packages(dockerfile_contents, cutoff_date)
 
     tmp_dockerfile_path = tmp_path / "Dockerfile"
     tmp_dockerfile_path.write_text(pinned_dockerfile_contents)
