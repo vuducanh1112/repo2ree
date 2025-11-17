dfc426ce09a7ae3beb4d2831fc5d8dbd161f5db2
Anh Duc Vu <AnhDuc.Vu@paluno.uni-due.de>
2025-11-17 13:22:18 +0000

    feat: added functions to build docker images and to save them locally

--- tests/integration/dockerfiles/test_pin_dockerfile.py
+++ tests/integration/dockerfiles/test_pin_dockerfile.py
@@ -1,22 +1,27 @@
-
 from datetime import datetime
-from pathlib import Path
 
+from repo2ree.dockerfile_utils.pin_versions import (
+    pin_dockerfile_base_image_and_packages,
+)
+from repo2ree.dockerfile_utils.build_image import build_docker_image
 
-from repo2ree.dockerfile_utils.pin_versions import pin_dockerfile_base_image_and_packages
-from repo2ree.dockerfile_utils.validate import is_dockerfile_buildable
 
 def test_pin_dockerfile(snapshot, resources_dir, tmp_path):
-
     dockerfile_path = resources_dir / "Dockerfile"
     dockerfile_contents = dockerfile_path.read_text()
 
-    cutoff_date = datetime(2025, 1, 1)
+    cutoff_date = datetime(2025, 10, 1)
 
-    pinned_dockerfile_contents = pin_dockerfile_base_image_and_packages(dockerfile_contents, cutoff_date)
+    pinned_dockerfile_contents = pin_dockerfile_base_image_and_packages(
+        dockerfile_contents, cutoff_date
+    )
 
     tmp_dockerfile_path = tmp_path / "Dockerfile"
     tmp_dockerfile_path.write_text(pinned_dockerfile_contents)
-    
+
     assert pinned_dockerfile_contents == snapshot
-    assert is_dockerfile_buildable(Path("pinned-Dockerfile"))
+
+    image = build_docker_image(tmp_path, "Dockerfile")
+    assert image is not None
+    if image:
+        image.remove(force=True)
