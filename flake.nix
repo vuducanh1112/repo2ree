592618b2fbe02ccb376832711e699cad0ea3ebeb
Anh Duc Vu <AnhDuc.Vu@paluno.uni-due.de>
2026-04-25 17:50:13 +0000

    feat(test): added playwright dependencies and config

--- flake.nix
+++ flake.nix
@@ -31,6 +31,11 @@
             kubectl
 
             renovate
+
+            playwright-driver.browsers
+            fontconfig
+            dejavu_fonts
+
           ];
 
           shellHook = ''
@@ -39,6 +44,10 @@
               echo "Node: $(node -v) | uv: $(uv --version) | kubectl: $(kubectl version --client --short 2>/dev/null || kubectl version --client)"
               export PS1="\[\033[1;34m\][nix-shell]\[\033[0m\] \w $ " # custom the shell prompt
             fi
+
+            export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+            export PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/chromium_headless_shell-1200/chrome-linux/headless_shell"
+            export FONTCONFIG_FILE=${pkgs.makeFontsConf { fontDirectories = [ pkgs.dejavu_fonts ]; }}
           '';
         };
       });
