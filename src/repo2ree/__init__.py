ac4bb5553590f0a1dbed5c9a66e346ee366d1c31
Anh Duc Vu <AnhDuc.Vu@paluno.uni-due.de>
2026-04-27 00:21:29 +0000

    chore: cleanup old sources

--- src/repo2ree/__init__.py
+++
@@ -1,8 +0,0 @@
-from repo2ree.cli.pin_dockerfile_versions import pin_dockerfile as pin_dockerfile
-
-from repo2ree.cli.pin_python_packages import (
-    pin_requirements_txt as pin_requirements_txt,
-)
-from repo2ree.cli.pin_python_packages import pin_pyproject_toml as pin_pyproject_toml
-
-from repo2ree.cli.profile_repository import profile_repository as profile_repository
--- src/repo2ree/__main__.py
+++
@@ -1,4 +0,0 @@
-from repo2ree.cli.cli import main
-
-if __name__ == "__main__":
-    main()
