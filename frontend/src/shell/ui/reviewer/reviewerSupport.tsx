1348da8bda42127e20dd5e3c220d9165f6dab3c0
Anh Duc Vu <AnhDuc.Vu@paluno.uni-due.de>
2026-05-06 14:32:01 +0000

    refactor(frontend): removed sibling reviewerSupport file

--- frontend/src/shell/ui/reviewer/reviewerSupport.tsx
+++
@@ -1,6 +0,0 @@
-export { MetaRow } from "./reviewerSupport/MetaRow";
-export { RvProvenanceChain } from "./reviewerSupport/RvProvenanceChain";
-export { RvStepCard } from "./reviewerSupport/RvStepCard";
-export { RvVerdictBanner } from "./reviewerSupport/RvVerdictBanner";
-export type { ReactivationParams, ReactivationStepKey } from "./reviewerSupport/reactivationSteps";
-export { REACTIVATION_STEPS } from "./reviewerSupport/reactivationSteps";
---
+++ frontend/src/shell/ui/reviewer/reviewerSupport/index.ts
@@ -0,0 +1,6 @@
+export { MetaRow } from "./MetaRow";
+export { RvProvenanceChain } from "./RvProvenanceChain";
+export { RvStepCard } from "./RvStepCard";
+export { RvVerdictBanner } from "./RvVerdictBanner";
+export type { ReactivationParams, ReactivationStepKey } from "./reactivationSteps";
+export { REACTIVATION_STEPS } from "./reactivationSteps";
